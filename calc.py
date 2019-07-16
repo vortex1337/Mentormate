ad4075242e43bd5391ace06b971f1733cab19989
Petar Dobrinov <petardobrinov95@gmail.com>
2019-07-16 16:54:14 +0300

    Calculator

---
+++ calc.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a+b
+def subtract(a, b):
+    return a-b
+def multiply(a, b):
+    return a*b
+def divide(a, b):
+    return a/b
+def square(a):
+    return a*a
+def qubic(a):
+    return a*a*a
